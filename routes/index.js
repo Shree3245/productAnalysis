141b35b1f4bdd9afbb9e7101832201dce001dc81
Abhishek Kasireddy <Akasireddy99@gmail.com>
2018-07-29 07:21:37 +0530

    Added "node_modules/**/*" to .gitignore
    Updated gitignore to exclude all node_modules from syncing, which takes too much time to sync each commit. Just run "npm install" from root directory everytime you sync from the repo to update the dependecies on your local machine.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var firebase = require('firebase');
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 var router = express.Router();
+
+var httpRequest = new XMLHttpRequest();
+
+httpRequest.onreadystatechange = function (resp) {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        console.log("4");
+    } else if (httpRequest.readyState === 3) {
+        console.log("3");
+    } else if (httpRequest.readyState === 2) {
+        console.log("2");
+    } else if (httpRequest.readyState === 1) {
+        console.log("1");
+    } else if (httpRequest.readyState === 0) {
+        console.log("0");
+    } else {
+        console.log(httpRequest.status);
+    }
+};
 
 var config = {
     apiKey: "AIzaSyC67YlaL3PDKvlW7-GOX4t2K-5HvgrjanQ",
@@ -12,10 +31,10 @@ var config = {
 };
 firebase.initializeApp(config);
 
-router.use('/', function(req, res, next) {
+router.use('/', function (req, res, next) {
     var ongoingProjectsRef = firebase.database().ref('ongoingProjects');
     var ongoingProjectsObj;
-    ongoingProjectsRef.on("value", function(snapshot) {
+    ongoingProjectsRef.on("value", function (snapshot) {
         ongoingProjectsObj = snapshot.val();
         var ongoingList = [];
         for (key in ongoingProjectsObj) {
@@ -30,10 +49,10 @@ router.use('/', function(req, res, next) {
     });
 });
 
-router.use('/', function(req, res, next) {
+router.use('/', function (req, res, next) {
     var finishedProjectsRef = firebase.database().ref('finishedProjects');
     var finishedProjectsObj;
-    finishedProjectsRef.on("value", function(snapshot) {
+    finishedProjectsRef.on("value", function (snapshot) {
         finishedProjectsObj = snapshot.val();
         var finishedList = [];
         for (key in finishedProjectsObj) {
@@ -49,9 +68,22 @@ router.use('/', function(req, res, next) {
 });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    console.log("Ongoing Projects in 'res' >", res.locals.ongoingProjects);
-    res.render('index', {title: "JASA | Project Foundry", ongoingProjects: res.locals.ongoingProjects})
+router.get('/', function (req, res, next) {
+    fetch("http://sentiment.vivekn.com/api/text/",
+        {
+            headers: {
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept': '*/*',
+                'Connection': 'keep-alive',
+                'Content-Type': "application/x-www-form-urlencoded"
+            },
+            method: "POST",
+            body: JSON.stringify({ txt: 'hi'})
+        })
+        .then(function (res) { console.log(res) })
+        .catch(function (res) { console.log(res) });
+
+    res.render('index', { title: "JASA | Project Foundry", ongoingProjects: res.locals.ongoingProjects })
 });
 
 module.exports = router;
